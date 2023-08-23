04a606a638e281b1eeaa59fff867742e70e2f46b
Niko test <Lesnik6.6.6@mail.ru>
2023-08-23 11:42:39 +0300

    Commit1

--- src/test/java/ru/netology/sel/hw/TestOrder.java
+++ src/test/java/ru/netology/sel/hw/TestOrder.java
@@ -2,7 +2,6 @@ package ru.netology.sel.hw;
 
 import com.codeborne.selenide.Condition;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
